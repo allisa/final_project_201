f219dc5d1d24cf53db7c7e3fbf0a83f84e243ba2
alstof <alstof@gmail.com>
2018-08-21 14:00:27 -0700

    Fix minor typos in descriptions

--- js/locations.js
+++ js/locations.js
@@ -5,7 +5,7 @@ var locations = [
     address: '022 Odegaard, Seattle, WA',
     geo: '47.655, -122.308',
     website: 'https://www.washington.edu/',
-    shortDesc: 'The university's 703-acre main Seattle campus is situated in the University District above the Montlake Cut, within the urban Puget Sound region of the Pacific Northwest. The university has two additional campuses in Tacoma and Bothell. Overall, UW encompasses over 500 buildings and over 20 million gross square footage of space, including one of the largest library systems in the world with over 26 university libraries, as well as the UW Tower, lecture halls, art centers, museums, laboratories, stadiums, and conference centers. The university offers bachelor's, master's and doctoral degrees through 140 departments in various colleges and schools, sees about 46,000 in total student enrollment every year, and functions on a quarter system.'
+    shortDesc: 'The university\'s 703-acre main Seattle campus is situated in the University District above the Montlake Cut, within the urban Puget Sound region of the Pacific Northwest. The university has two additional campuses in Tacoma and Bothell. Overall, UW encompasses over 500 buildings and over 20 million gross square footage of space, including one of the largest library systems in the world with over 26 university libraries, as well as the UW Tower, lecture halls, art centers, museums, laboratories, stadiums, and conference centers. The university offers bachelor's, master's and doctoral degrees through 140 departments in various colleges and schools, sees about 46,000 in total student enrollment every year, and functions on a quarter system.'
   },
   {
     name: 'Space Needle',
